03ba8a9701774fc0931e3995e86f7800de7ac033
Ahasanul Rabbi Khan <me.ahsanul01@gmail.com>
2025-07-31 04:58:39 +0200

    page bug fix

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { Signin } from './pages/Signin'
-import { Signup } from './pages/Signup'
-import { Profile } from './pages/Profile'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile';
+import NoPage from './pages/NoPage';
 
-export default function App() {
-  return <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/sign-in' element={<Signin />} />
-            <Route path='/sign-up' element={<Signup />} />
-            <Route path='/profile' element={<Profile />} />
-          </Routes>
-  
-  
-  </BrowserRouter>
-  
+const App = () => {
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path="*" element={<NoPage />} />
+    </Routes>
+    </>
+  )
 }
+
+export default App
--- webapp/src/pages/Signup.jsx
+++ webapp/src/pages/Signup.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const Signup = () => {
+const Signup = () => {
   return (
     <div>Signup</div>
   )
 }
+
+export default Signup
